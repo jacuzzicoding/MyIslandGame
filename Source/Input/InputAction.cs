49bded3fefd030131929d3004697aa9217fc7099
Brock Jenkinson <brockjenkinson@gmail.com>
2025-03-02 19:40:56 -0500

    Remove MouseButton enumeration from InputAction for code cleanup

--- Source/Input/InputAction.cs
+++ Source/Input/InputAction.cs
@@ -196,44 +196,4 @@ namespace MyIslandGame.Input
         RightX, RightY
     }
 
-    /// <summary>
-    /// Enumeration of mouse buttons and actions.
-    /// </summary>
-    public enum MouseButton
-    {
-        /// <summary>
-        /// The left mouse button.
-        /// </summary>
-        Left,
-        
-        /// <summary>
-        /// The right mouse button.
-        /// </summary>
-        Right,
-        
-        /// <summary>
-        /// The middle mouse button (scroll wheel click).
-        /// </summary>
-        Middle,
-        
-        /// <summary>
-        /// The first extended mouse button.
-        /// </summary>
-        XButton1,
-        
-        /// <summary>
-        /// The second extended mouse button.
-        /// </summary>
-        XButton2,
-        
-        /// <summary>
-        /// Scroll wheel up action.
-        /// </summary>
-        ScrollUp,
-        
-        /// <summary>
-        /// Scroll wheel down action.
-        /// </summary>
-        ScrollDown
-    }
 }
