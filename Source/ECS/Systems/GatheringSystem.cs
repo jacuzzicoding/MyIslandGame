34d77f89904cddc7f1560b3a33d8b3088e834ea6
Brock Jenkinson <brockjenkinson@gmail.com>
2025-03-02 19:41:02 -0500

    Add import for Microsoft.Xna.Framework.Input to GatheringSystem for keyboard input handling

--- Source/ECS/Systems/GatheringSystem.cs
+++ Source/ECS/Systems/GatheringSystem.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input; // Add this import for Keys
 using MyIslandGame.Core.Resources;
 using MyIslandGame.ECS.Components;
 using MyIslandGame.Input;
