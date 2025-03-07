dc8db3adf38c61ebd7273fd639a6d1bc78904ae4
Brock Jenkinson <brockjenkinson@gmail.com>
2025-03-07 15:24:04 -0500

    Add Core.Resources namespace to CraftingUI for resource management

--- Source/UI/CraftingUI.cs
+++ Source/UI/CraftingUI.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MyIslandGame.Crafting;
+using MyIslandGame.Core.Resources;
 using MyIslandGame.ECS;
 using MyIslandGame.ECS.Components;
 using MyIslandGame.Input;
